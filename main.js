0123112b3387c01440675e38773db349c2660c73
Jae Hee <loseeya077777@gmail.com>
2021-11-14 21:45:16 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+$('.card-img').slick({
+  centerMode: true,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
